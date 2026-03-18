9540edebf2f68601f0bfa3331579850331054f23
llpp <dfa@dsf.com>
2026-03-18 12:50:22 +0800

    drawer sample

---
+++ lib/app/modules/ApiUsageTemplate/controllers/api_usage_template_drawer_controller.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+import '../../../../util/Log.dart';
+import '../../../base/base_controller.dart';
+
+class TemplateDrawerController extends BaseController {
+
+
+  @mustCallSuper
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @mustCallSuper
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @mustCallSuper
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+}
--- lib/app/modules/ApiUsageTemplate/views/api_usage_template_view.dart
+++ lib/app/modules/ApiUsageTemplate/views/api_usage_template_view.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
 import '../../../component/app_button.dart';
 import '../../../component/my_app_bar.dart';
 import '../controllers/api_usage_template_controller.dart';
+import 'drawer_api.dart';
 
 class ApiUsageTemplateView extends GetView<ApiUsageTemplateController> {
   const ApiUsageTemplateView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(
-        title: 'ApiUsageTemplateView',
+      appBar: MyAppBar(title: 'ApiUsageTemplateView'),
+      // Scaffold嵌套 保证抽屉只在 body部分，不会遮挡appBar
+      body: Scaffold(
+        drawer: DrawerApiView(),
+        body: Container(
+          width: double.infinity,
+          color: Color(0xff84abf6),
+          child: Builder(
+            builder: (BuildContext context) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  AppButton(
+                    padding: EdgeInsets.all(10),
+                    text: '打开抽屉',
+                    onClick: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
       ),
-      body: Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Color(0xff84abf6),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-
-        ],
-      ),
-    ),
     );
   }
 }
