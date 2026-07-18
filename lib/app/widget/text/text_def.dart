946ceb4824266e49f4eb78c89216cc986fed1606
nikilaus  <nikilaus@example.com>
2026-07-18 16:47:42 +0400

    优化

--- lib/app/widget/text/text_def.dart
+++ lib/app/widget/text/text_def.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_comm/screen_info.dart';
 
 const style12 = TextStyle(fontSize: 12);
 
@@ -11,4 +12,18 @@ class AppText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text(text, style: TextStyle(fontSize: 12, color: Color(0x99202020)));
   }
+}
+
+final defTextStyle = TextStyle(fontSize: 13.w, color: Colors.black, fontWeight: FontWeight.w400);
+
+class TextWidget extends Text {
+  const TextWidget(super.data, {super.key});
+
+  TextWidget.def(
+    super.data, {
+    super.key,
+    super.textAlign = TextAlign.center,
+    super.maxLines = 1,
+    super.overflow = TextOverflow.ellipsis,
+  }) : super(style: defTextStyle);
 }
